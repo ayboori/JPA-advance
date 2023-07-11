87160c1e72fa1c8592b7a48696799c70bf21758c
ayoung <ayoung3052@gmail.com>
2023-07-12 01:43:03 +0900

    중간테이블 Order 주문일 추가

--- src/main/java/com/sparta/jpaadvance/JpaAdvanceApplication.java
+++ src/main/java/com/sparta/jpaadvance/JpaAdvanceApplication.java
@@ -2,8 +2,10 @@ package com.sparta.jpaadvance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 있어야 Timestamp 사용 가능!
 public class JpaAdvanceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sparta/jpaadvance/entity/Order.java
+++ src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -3,11 +3,16 @@ package com.sparta.jpaadvance.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class) // 이거 있어야 TimeStamp 가능
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +25,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    // 중간 테이블에서 주문 시간을 얻고 싶어 한다면?
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime orderDate;
 }
