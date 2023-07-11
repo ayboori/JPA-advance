d05c5b170c550d8eafae0228ea835288eb66ac91
ayoung <ayoung3052@gmail.com>
2023-07-11 20:33:18 +0900

    @OneToMany 단방향 및 조회 테스트

--- src/main/java/com/sparta/jpaadvance/repository/UserRepository.java
+++ src/main/java/com/sparta/jpaadvance/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.sparta.jpaadvance.repository;
 
-import com.sparta.jpaadvance.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.sparta.jpaadvance.entity.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 }
