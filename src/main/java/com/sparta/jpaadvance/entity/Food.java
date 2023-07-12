a9161a45ea7a4a8b68cef0f4f0fec05a08e3c701
ayoung <ayoung3052@gmail.com>
2023-07-12 14:59:51 +0900

    지연 로딩과 즉시 로딩

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -18,6 +18,7 @@ public class Food {
     private String name;
     private double price;
 
-    @OneToMany(mappedBy = "food")
-    private List<Order> orderList = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- src/main/java/com/sparta/jpaadvance/repository/UserRepository.java
+++ src/main/java/com/sparta/jpaadvance/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sparta.jpaadvance.entity.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByName(String name);
 }
